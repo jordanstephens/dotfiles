e81a307807549fa05687f65ab417c9320c37cd65
Jordan Stephens <iam@jordanstephens.net>
2013-08-05 22:58:58 +0000

    dont symlink readme

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ FILES=$HOME/dotfiles/*
 for file in $FILES
 do
     name=$(basename $file)
-    if [ $name != $(basename $0) ]
+    if [ $name != $(basename $0) ] && [ $name != "readme.md" ]
     then
         ln -sv $file $HOME/.$name
     fi
