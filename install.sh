c33bc8c1963885151dc6f12feb7d32d1cdff55cf
Jordan Stephens <iam@jordanstephens.net>
2013-08-08 19:26:32 -0400

    install script will update submodules and switch to zsh for you

--- install.sh
+++ install.sh
@@ -2,12 +2,27 @@
 
 FILES=$HOME/dotfiles/*
 
+echo "Symlinking config files to $HOME"
 for file in $FILES
 do
     name=$(basename $file)
     if [ $name != $(basename $0) ] && [ $name != "readme.md" ]
     then
-        ln -sv $file $HOME/.$name
+	ln -sv $file $HOME/.$name
     fi
 done
+
+
+echo "Initializing and updating git submodules"
+git submodule update --init
+
+
+if [ -f /bin/zsh ]
+then
+	echo "Changing login shell to /bin/zsh"
+	chsh -s /bin/zsh # change login shell to zsh
+  /bin/zsh # use zsh now
+else
+	echo "Could not find /bin/zsh, login shell will remain $SHELL"
+fi
 
--- readme.md
+++ readme.md
@@ -9,16 +9,8 @@ Clone the repo and run the install script from your home directory
     cd dotfiles
     ./install.sh
 
-Update plugin submodules
-
-    git submodule update --init
-
-You will want to change your login shell to zsh
-
-    chsh -s /bin/zsh
-    
 * * *
-    
+
 **Important:** Don't forget to update the `[user]` stanza in `.gitconfig` so that your information is reflected rather than mine
 
     [user]
